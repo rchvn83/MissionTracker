f5d7ab330c75fdb7c6bd6a9d14636df20198396c
rchvn83 <109677545+rchvn83@users.noreply.github.com>
2022-07-24 21:05:54 +0200

    Update mission_tracker.php

--- mission_tracker.php
+++ mission_tracker.php
@@ -8,6 +8,9 @@
   </head>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   <body>
+
+     <!-- Navbar - Mission Tracker -->
+
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Mission Tracker</a>
@@ -17,23 +20,16 @@
 
       <div class="collapse navbar-collapse" id="navbarsExample03">
         <ul class="navbar-nav me-auto mb-2 mb-sm-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Lien</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Désactivée</a>
-          </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Voir plus</a>
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Entreprises</a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">Entreprise1</a></li>
+              <li><a class="dropdown-item" href="#">Entreprise2</a></li>
+              <li><a class="dropdown-item" href="#">Entreprise3</a></li>
             </ul>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Missions</a>
         </ul>
         <form role="search">
           <input class="form-control" type="search" placeholder="Rechercher" aria-label="Search">
@@ -42,35 +38,37 @@
     </div>
   </nav>
 
+   <!-- List - Entreprise -->
+
   <div class="list-group w-auto">
   <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-    <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+    <img src="images/entreprise_mini_icon.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
     <div class="d-flex gap-2 w-100 justify-content-between">
       <div>
-        <h6 class="mb-0">List group item heading</h6>
-        <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
+        <h6 class="mb-0">Entreprise1</h6>
+        <p class="mb-0 opacity-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
-      <small class="opacity-50 text-nowrap">now</small>
+      <small class="opacity-50 text-nowrap"></small>
     </div>
   </a>
   <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-    <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+    <img src="images/entreprise_mini_icon.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
     <div class="d-flex gap-2 w-100 justify-content-between">
       <div>
-        <h6 class="mb-0">Another title here</h6>
-        <p class="mb-0 opacity-75">Some placeholder content in a paragraph that goes a little longer so it wraps to a new line.</p>
+        <h6 class="mb-0">Entreprise2</h6>
+        <p class="mb-0 opacity-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
-      <small class="opacity-50 text-nowrap">3d</small>
+      <small class="opacity-50 text-nowrap"></small>
     </div>
   </a>
   <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-    <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+    <img src="images/entreprise_mini_icon.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
     <div class="d-flex gap-2 w-100 justify-content-between">
       <div>
-        <h6 class="mb-0">Third heading</h6>
-        <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
+        <h6 class="mb-0 text-nowrap">Entreprise3</h6>
+        <p class="mb-0 opacity-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
-      <small class="opacity-50 text-nowrap">1w</small>
+      <small class="opacity-50 text-nowrap"></small>
     </div>
   </a>
 </div>
